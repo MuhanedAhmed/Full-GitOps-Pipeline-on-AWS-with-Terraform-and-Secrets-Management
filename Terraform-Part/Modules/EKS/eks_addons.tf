9d116b7bf039a541512fa2fa5545b6b0b6eb94b9
MuhanedAhmed <MuhannedAhmedw@gmail.com>
2025-06-03 20:40:00 +0300

    fix(terraform): fix permissions for ebs and vpc-cni addons for the eks cluster

--- Terraform-Part/Modules/EKS/eks_addons.tf
+++ Terraform-Part/Modules/EKS/eks_addons.tf
@@ -8,8 +8,15 @@ resource "aws_eks_addon" "gp_kube-proxy" {
 }
 
 resource "aws_eks_addon" "gp_vpc-cni" {
-  cluster_name = aws_eks_cluster.gp_eks_cluster.name
-  addon_name   = "vpc-cni"
+  cluster_name             = aws_eks_cluster.gp_eks_cluster.name
+  addon_name               = "vpc-cni"
+  service_account_role_arn = aws_iam_role.gp_vpc_cni_addon_role.arn
+}
+
+resource "aws_eks_addon" "gp_aws-ebs-csi-driver" {
+  cluster_name             = aws_eks_cluster.gp_eks_cluster.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.gp_ebs_addon_role.arn
 }
 
 resource "aws_eks_addon" "gp_eks-node-monitoring-agent" {
@@ -25,11 +32,6 @@ resource "aws_eks_addon" "gp_coredns" {
 resource "aws_eks_addon" "gp_eks-pod-identity-agent" {
   cluster_name = aws_eks_cluster.gp_eks_cluster.name
   addon_name   = "eks-pod-identity-agent"
-}
-
-resource "aws_eks_addon" "gp_external-dns" {
-  cluster_name = aws_eks_cluster.gp_eks_cluster.name
-  addon_name   = "external-dns"
 }
 
 resource "aws_eks_addon" "gp_metrics-server" {
