9f4e9d7438ef37ad3feb82f7e962f6b1d4bb3ee5
MohamedHaro0on <mohamedharoon286@gmail.com>
2025-06-10 09:33:11 +0300

    Feat: bastion host User Data

--- Terraform-Part/Modules/EC2/ec2.tf
+++ Terraform-Part/Modules/EC2/ec2.tf
@@ -27,10 +27,11 @@ resource "aws_instance" "my_instance" {
   private_ip                  = each.value.instance_private_ip
   associate_public_ip_address = each.value.has_public_ip
   vpc_security_group_ids      = each.value.security_group_ids
-  user_data                   = each.value.user_data
+  # user_data                   = each.value.user_data
   iam_instance_profile        = each.value.instance_profile
   key_name                    = var.instance_key_pair
-  user_data_replace_on_change = true
+  # user_data_replace_on_change = true
+  user_data = base64decode(file("${path.module}/scripts/bastion.sh"))
 
   tags = {
     Name       = each.key
---
+++ Terraform-Part/Modules/EC2/scripts/bastion.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt-get update
+
+# Install Git
+sudo apt-get install -y git
+
+# Install Ansible
+sudo apt-get install -y ansible
+
+# Install Ansible community.kubernetes collection
+ansible-galaxy collection install community.kubernetes
+
+# Download and install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+
+git clone https://github.com/MuhanedAhmed/Full-GitOps-Pipeline-on-AWS-with-Terraform-and-Secrets-Management.git
+cd Full-GitOps-Pipeline-on-AWS-with-Terraform-and-Secrets-Management
+
+ansible-playbook ./Full-GitOps-Pipeline-on-AWS-with-Terraform-and-Secrets-Management/Ansible-Part/eks-bastion-setup/playbook.yml \
+-i ./Full-GitOps-Pipeline-on-AWS-with-Terraform-and-Secrets-Management/Ansible-Part/eks-bastion-setup/inventory.ini
+
+# Verify installations
+echo "Verifying installations..."
+
+git --version
+ansible --version
+kubectl version --client
+helm version
+
+echo "Installation complete!"
